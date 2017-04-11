e0a7132e03c7044da8ae927d0bad7dad1820fbbd
Rashmit <rashmit.rathod@gmail.com>
2017-04-11 12:09:34 +0530

    updated business logic

---
+++ src/main/java/com/cybage/docker/HelloDockerApplication.java
@@ -0,0 +1,12 @@
+package com.cybage.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellodockerApplication.class, args);
+	}
+}
